3044f2f20de6ad6d8dcad18af449420d539f7776
Vũ Quốc Hoàng <hoang.vuquoc@pixta.co.jp>
2025-05-13 08:01:17 +0000

    feat: Add isort and black

--- init.lua
+++ init.lua
@@ -718,6 +718,8 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'markdownlint',
+        'isort',
+        'black',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -770,7 +772,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'ruff' },
+        python = { 'isort', 'black' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
